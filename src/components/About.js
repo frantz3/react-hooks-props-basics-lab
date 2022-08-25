e91fed53c8891dd746c10e00474ae70a740d0d6c
frantzmoseau <frantzmoseau35@gmail.com>
2022-08-24 23:55:17 -0400

    done

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About(props) {
       <h2>About Me</h2>
       {props.bio && props.bio.length > 1 ? <p>{props.bio}</p>: null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />     
-      <Links github={props.links.github} linkedIn={props.links.linkedIn} />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
   
